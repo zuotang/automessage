20a9cbc84160041b3eaf58c35d6e4deaa3eb6d92
ZuoTang <zuo.tang@outlook.com>
2026-03-31 17:32:18 +0800

    release

--- main.js
+++ main.js
@@ -1,37 +1,7 @@
-
-auto.waitFor();
 "auto";
 
 const PROJECT_DIR = files.path(".");
-
-function r(relPath) {
-    const fullPath = files.join(PROJECT_DIR, relPath);
-    log("准备加载模块: " + fullPath);
-    log("文件是否存在: " + files.exists(fullPath));
-
-    if (files.exists(fullPath)) {
-        log("文件内容预览: ");
-        log(files.read(fullPath));
-    }
-
-    return require(fullPath);
-}
-
-try {
-    log("当前目录: " + PROJECT_DIR);
-    log("main.js 存在: " + files.exists(files.join(PROJECT_DIR, "main.js")));
-    log("utils.js 存在: " + files.exists(files.join(PROJECT_DIR, "lib/utils.js")));
-    log("task.js 存在: " + files.exists(files.join(PROJECT_DIR, "lib/task.js")));
-
-    const utils = r("lib/utils.js");
-
-    utils.randomSleep(100);
-} catch (e) {
-    console.error(e);
-    toastLog("main.js 运行异常: " + e.message);
-}
-
-
+const utils = require(files.join(PROJECT_DIR, "lib/utils.js"));
 
 let running = false;
 let worker = null;
@@ -94,7 +64,7 @@ function stopTask() {
     toast("已停止");
 }
 
-// 你的主逻辑
+// 主逻辑
 function runTask() {
     app.launchPackage("com.zhiliaoapp.musically");
 
@@ -109,16 +79,11 @@ function runTask() {
     } else {
         toast("没找到");
     }
-    //等待1s
+
     utils.randomSleep(1000);
-    //模拟上滑
     utils.swipeUpPageHuman();
-    //等待1s
     utils.randomSleep(1000);
 }
 
-
-
-
-// 防止脚本直接退出
+// 防止退出
 setInterval(() => {}, 1000);
---
+++ task.js
@@ -0,0 +1,6 @@
+module.exports = {
+    run: function () {
+        log("task.run() 执行中");
+        toastLog("任务执行成功");
+    }
+};
