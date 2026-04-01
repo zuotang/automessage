5e0cda795c403de066ebfbd4ea70e5b7716fd1a3
ZuoTang <zuo.tang@outlook.com>
2026-04-01 12:07:40 +0800

    release

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function collectN50Items() {
 // 悬浮窗
 let w = floaty.window(
     <frame>
-        <button id="toggle" text="开启77" w="60" h="40" bg="#AA00CC66"/>
+        <button id="toggle" text="开启78" w="60" h="40" bg="#AA00CC66"/>
     </frame>
 );
 
